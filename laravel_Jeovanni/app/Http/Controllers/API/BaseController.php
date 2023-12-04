40e23b700af42388b007f850205872c54c408f73
JeoVn <132755008+JeoVn@users.noreply.github.com>
2023-12-04 13:10:13 +0700

    ppt 25

---
+++ laravel_Jeovanni/app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller as Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function sendResponse($result, $message){
+        $response =[
+            'success'=> true,
+            'data' => $result,
+            'message' => $message,
+        ];
+        return response() -> json($response,200);
+    }
+
+    public function sendError($error, $errormessages = [], $code =404){
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if (!empty($errormessages)){
+            $response['data'] = $errormessages;
+        }
+        return response()->json($response, $code);
+    }
+}
